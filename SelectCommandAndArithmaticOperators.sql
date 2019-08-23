6d68cd216850bf3e16c5b68abb27fd7ea3256534
Roy Nunez <roynunez273@gmail.com>
2019-08-22 20:06:03 -0400

    MySQL update

---
+++ SelectCommandAndArithmaticOperators.sql
@@ -0,0 +1,19 @@
+use sakila;
+show tables;
+select * from actor;
+
+
+-- Projection 
+
+
+-- Selection 
+
+
+
+
+-- Joining 
+
+
+
+-- arithmetic
+SELECT actor_id*100+1,first_name,last_name FROM actor;
