10fb929f7b07bf916baea68db80d7b32105f261a
Roy Nunez <roynunez273@gmail.com>
2019-08-22 20:08:15 -0400

    Update SelectCommandAndArithmaticOperators.sql

--- SelectCommandAndArithmaticOperators.sql
+++ SelectCommandAndArithmaticOperators.sql
@@ -1,19 +1,18 @@
 use sakila;
 show tables;
-select * from actor;
-
+select * from actor; 
 
 -- Projection 
-
+select first_name, last_name from actor;
 
 -- Selection 
+select * from actor where first_name = 'Penelope';
 
-
-
-
--- Joining 
-
-
+-- Joining
+select * from actor_info;
+SELECT * FROM actor,actor_info WHERE actor.first_name = actor_info.first_name;
 
 -- arithmetic
 SELECT actor_id*100+1,first_name,last_name FROM actor;
+
+select 20+200, 35*3,  3+8;
